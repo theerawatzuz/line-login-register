9d55ceffba7f840eddfc84ed45af8c9f803db8ec
silimechok <theerawat.silimechok@accenture.com>
2024-05-08 20:48:25 +0700

    AddStatus

--- app/line/page.js
+++ app/line/page.js
@@ -32,7 +32,7 @@ const handleLogin = async() => {
             <div class="flex flex-col items-center pb-10">
                 <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src={profile.pictureUrl} alt="Pic Profile"/>
                 <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{profile.displayName}</h5>
-                <span class="text-sm text-gray-500 dark:text-gray-400">Hello from server.</span>
+                <span class="text-sm text-gray-500 dark:text-gray-400">{profile.statusMessage}</span>
                 <div class="flex mt-4 md:mt-6">
                     <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Log out</a>
                 </div>
