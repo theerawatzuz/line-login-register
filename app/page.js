730fc6a2252d49f214a7257317945e3ba2e0866d
silimechok <theerawat.silimechok@accenture.com>
2024-05-09 02:42:45 +0700

    AddGetAllLoginLog

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
-// 'use client';
+'use client';
 
 // import Link from 'next/link';
 // import Image from 'next/image';
 // import React, {useEffect, useState } from 'react';
 // import liff from '@line/liff'
 
-
+import React, { useState, useEffect } from 'react';
 import Line from './line/page';
+import axios from 'axios';
 
 export default function Home() {
 // useEffect(() => {
@@ -27,6 +28,33 @@ export default function Home() {
 //   }
 // }
 
+const [userData, setUserData] = useState([]);
+
+useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('https://ap-southeast-1.aws.data.mongodb-api.com/app/application-1-dwdap/endpoint/lineLogCallUserLogin');
+            setUserData(response.data);
+        } catch(error) {
+            console.log('Error fetching data:', error);
+        }
+    };
+
+    fetchData();
+}, []);
+
+const formatTimestamp = (timestamp) => {
+    const options = {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZoneName: 'short',
+    };
+    return new Date(timestamp).toLocaleString('en-GB', options);
+};
+
 
   return (
     <section class="bg-gray-50 dark:bg-gray-900">
@@ -35,28 +63,30 @@ export default function Home() {
    <Line/>
      <div class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
     <div class="flex items-center justify-between mb-4">
-        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest Customers</h5>
+        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest login</h5>
         {/* <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
             View all
         </a> */}
    </div>
    <div class="flow-root">
         <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
-            <li class="pt-3 pb-0 sm:pt-4">
+            {userData.map(user => (
+            <li key={user._id} class="pt-3 pb-3 sm:pt-4">
                 <div class="flex items-center ">
                     <div class="flex-shrink-0">
-                        <img class="w-8 h-8 rounded-full" src="" alt="Thomas image"/>
+                        <img class="w-8 h-8 rounded-full" src={user.userPic} alt={`${user.userName} image`}/>
                     </div>
                     <div class="flex-1 min-w-0 ms-4">
                         <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                            Thomes Lean
+                            {user.userName}
                         </p>
                     </div>
                     <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                        $2367
+                    {formatTimestamp(user.timeStamp)}
                     </div>
                 </div>
             </li>
+            ))}
         </ul>
    </div>
 </div>
