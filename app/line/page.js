ccfe07450422f19e3448a186303b962e7ba48a1f
silimechok <theerawat.silimechok@accenture.com>
2024-05-08 20:10:16 +0700

    ChangeToLinePage

--- app/line/page.js
+++ app/line/page.js
@@ -1,9 +1,29 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react'
+import liff from "@line/liff"
 
 const Line = () => {
+useEffect(() => {
+    liff.init({ liffId: '2004878671-5kGmazLb'})
+    .then(() => {
+        handleLogin()
+    });
+},[]);
+ 
+const handleLogin = async() => {
+    try{
+        //code
+        const profile = await liff.getProfile()
+        console.log(profile)
+    }catch(err){
+        console.log(err)
+    }
+} 
+
     return(
-        <div>Line</div>
+        <>Line</>
     )
 }
 
-export default Line
+export default Line;
--- app/page.js
+++ app/page.js
@@ -1,31 +1,31 @@
-'use client';
+// 'use client';
 
-import Link from 'next/link';
-import Image from 'next/image';
-import React, {useEffect, useState } from 'react';
-import liff from '@line/liff'
+// import Link from 'next/link';
+// import Image from 'next/image';
+// import React, {useEffect, useState } from 'react';
+// import liff from '@line/liff'
 
 
 import Line from './line/page';
 
 export default function Home() {
-useEffect(() => {
-  liff.init({ liffId: '2004878671-5kGmazLb'})
-  .then(() => {
-    //code
-    handleLogin()
-  });
-},[]);
+// useEffect(() => {
+//   liff.init({ liffId: '2004878671-5kGmazLb'})
+//   .then(() => {
+//     //code
+//     handleLogin()
+//   });
+// },[]);
 
-const handleLogin = async() => {
-  try{
-        //code
-        const profile = await liff.getProfile()
-        console.log(profile)
-  }catch(err){
-    console.log(err)
-  }
-}
+// const handleLogin = async() => {
+//   try{
+//         //code
+//         const profile = await liff.getProfile()
+//         console.log(profile)
+//   }catch(err){
+//     console.log(err)
+//   }
+// }
 
 
   return (
