6c5d0dd734e54dc7a1233d4251636911d9d3fc56
silimechok <theerawat.silimechok@accenture.com>
2024-05-08 21:22:52 +0700

    AddRedirect

--- app/line/page.js
+++ app/line/page.js
@@ -1,10 +1,12 @@
 'use client';
 
-import React, { useEffect, useState } from 'react'
-import liff from "@line/liff"
+import React, { useEffect, useState } from 'react';
+import liff from "@line/liff";
+// import { useHistory } from 'react-router-dom';
 
 const Line = () => {
 const [profile, setProfile] = useState(null);
+// const history = useHistory();
 
 useEffect(() => {
     liff.init({ liffId: '2004878671-5kGmazLb'})
@@ -23,6 +25,18 @@ const handleLogin = async() => {
     }
 } 
 
+const handleLogout = async () => {
+    try{
+        liff.logout();
+        setProfile(null);
+        window.location.href = '/login';
+        // history.push('/login');
+    }catch(err){
+        console.log(err)
+    }
+    
+}
+
     return(
         <>
         {profile && (
@@ -34,7 +48,7 @@ const handleLogin = async() => {
                 <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{profile.displayName}</h5>
                 <span class="text-sm text-gray-500 dark:text-gray-400">{profile.statusMessage}</span>
                 <div class="flex mt-4 md:mt-6">
-                    <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Log out</a>
+                    <button onClick={handleLogout} class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Log out</button>
                 </div>
             </div>
         </div>
